506d3704f70ffe0350c9f47bab28612990fb31cf
Raghib2004 <raghibaa4444@gamil.com>
2025-11-11 16:38:20 +0530

    LeetCode Question_1

---
+++ Q1_Two_Sum_.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for ( int i = 0 ; i < nums.size() ; i ++ ){
+            for ( int j = i+1 ; j < nums.size() ; j ++ ){
+                int curr_Sum = nums[i] + nums[j] ;
+                if (target == curr_Sum){
+                       return {i,j};
+                }
+                curr_Sum = 0;
+            }
+        }   
+         return {-1,-1};
+    }
+  };
